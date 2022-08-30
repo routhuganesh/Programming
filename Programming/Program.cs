f73390f8644b38bc70f1abfa0495c60ad8da957a
routhuganesh <routhuganesh35@gmail.com>
2022-08-30 22:13:28 +0530

    Add project files.

---
+++ Programming/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+public class Test
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+    public static void Main()
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student() { Id = 1, Name = "Ganesh",Department = "Developer"},
+            new Student() { Id =2, Name = "Elon",Department = "Programmer"},
+            new Student() {Id =3, Name = "Mark", Department = "Developer"}
+        };
+        var studentlist = students.Select(x=> new {x.Id, x.Name,x.Department}).ToList();
+        int count = students.Sum(x => { if (x.Department == "Developer") return 2; else return 1; });
+        Console.WriteLine(count);
+        var studentList = students.ToList();
+        bool check = students.Any(x => x.Department == "Developer");
+        Console.WriteLine(check);
+        foreach(var student in studentList)
+        {
+            Console.WriteLine(student); 
+        }
+        
+    }
+} 
